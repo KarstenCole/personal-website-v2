8bc23518a0cfef5b6eb9d0eca9aabd2e93716be9
Karsten <kcole1371@bths.edu>
2024-03-20 18:35:03 -0400

    updated project cards

--- src/pages/projects/ProjectCard.tsx
+++ src/pages/projects/ProjectCard.tsx
@@ -2,19 +2,20 @@ import SubHeader from "../../components/ui/SubHeader.tsx";
 
 interface Props {
   title?: string;
-  tags?: string[];
+  tagLine1: string[];
+  tagLine2?: string[];
   img?: string;
 }
 
-const ProjectCard = ({ title, tags, img }: Props) => {
+const ProjectCard = ({ title, tagLine1, tagLine2, img }: Props) => {
   return (
-    <div className="w-[500px] bg-[#0f0f0f] text-center rounded-xl">
+    <div className="w-[500px] bg-[#0f0f0f] text-center rounded-xl ">
       <SubHeader textStyle="text-secondary mt-1">{title}</SubHeader>
       <div className="ml-7 mr-7 mt-1 p-[7px] rounded-lg bg-primary-gradient">
         <img src={img} alt={title} className="bg-cover rounded-md" />
       </div>
-      <div className="flex m-7">
-        {tags?.map((item, index) => (
+      <div className="flex m-7 justify-center">
+        {tagLine1.map((item, index) => (
           <div
             className="pr-1 pl-1 ml-2 mr-2 pt-[2px] pb-[2px] pl-3 pr-3 rounded-full bg-light-background text-primary font-manrope"
             key={index}
@@ -23,6 +24,18 @@ const ProjectCard = ({ title, tags, img }: Props) => {
           </div>
         ))}
       </div>
+      {tagLine2 && (
+        <div className="flex m-7 justify-center">
+          {tagLine2?.map((item, index) => (
+            <div
+              className="pr-1 pl-1 ml-2 mr-2 pt-[2px] pb-[2px] pl-3 pr-3 rounded-full bg-light-background text-primary font-manrope"
+              key={index}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -4,20 +4,25 @@ import Title from "./Title.tsx";
 
 const Projects = () => {
   return (
-    <div className="flex items-center flex-col mb-40">
+    <div className="flex items-center flex-col mb-20">
       <UpArrow style="mt-[73px]"></UpArrow>
       <Title textStyle="mt-[73px]">Projects</Title>
-      <div className="grid grid-cols-2 gap-20 mt-20 z-[1]">
-        <ProjectCard
-          title="Project 1"
-          img="src/assets/ExProject.png"
-          tags={["react", "tailwindcss", "gsap", "typescript"]}
-        ></ProjectCard>
-        <ProjectCard
-          title="Project 2"
-          img="src/assets/ExProject.png"
-          tags={["react", "tailwindcss", "gsap", "typescript"]}
-        ></ProjectCard>
+      <div className="flex flex-row mt-20 z-[1]">
+        <div className="flex flex-col mr-20">
+          <ProjectCard
+            title="Project 1"
+            img="src/assets/ExProject.png"
+            tagLine1={["React", "TailwindCSS", "GSAP", "TypeScript"]}
+          ></ProjectCard>
+        </div>
+        <div className="flex flex-col">
+          <ProjectCard
+            title="Project 2"
+            img="src/assets/ExProject.png"
+            tagLine1={["React", "TailwindCSS", "GSAP", "TypeScript"]}
+            tagLine2={["HTML", "CSS", "JavaScript", "GSAP"]}
+          ></ProjectCard>
+        </div>
       </div>
     </div>
   );
