fb637460c393270587316d037e2739376cb2614f
Karsten <kcole1371@bths.edu>
2024-05-30 10:40:33 -0400

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/personal-website-v2/',
   plugins: [react()],
   css: {
     postcss: {
